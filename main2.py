1379469d1c07c97d913f8d1d9d288aa4b2ad0878
Abraham <abanfo81@gmail.com>
2023-08-13 15:21:05 +0200

    comment fixed missed columns

--- main2.py
+++ main2.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from anonymizedf.anonymizedf import anonymize
 
+
+# all code works
+
 # def anonymize_data(input_file, output_file, column_mappings):
 #     # Import the data
 #     def __init__():
@@ -65,28 +68,97 @@ from anonymizedf.anonymizedf import anonymize
     
 # fake_df.save("fake_customers5.csv")
 
-class Anonymizer:
-    def __init__(self, input_file, output_file):
-        self.input_file = input_file
-        self.output_file = output_file
+# class Anonymizer:
+#     def __init__(self, input_file, output_file):
+#         self.input_file = input_file
+#         self.output_file = output_file
         
-    def anonymize_data(self):
-        df = pd.read_csv(self.input_file, delimiter="|")
-        an = anonymize(df)
+#     def anonymize_data(self):
+#         df = pd.read_csv(self.input_file, delimiter=";")
+#         print("data reading")
+#         an = anonymize(df)
         
-        fake_df = (
-            an
-            .fake_rakening("rekening_nummer", "tegenrekening_nummer", chaining=True)
-            .fake_categories("rekening_naam", "tegenrekening_naam", chaining=True)
-            .fake_categy("omschrijving", chaining=True)
-            # .show_data_frame()
-        )
+#         fake_df = (
+#             an
+#             .fake_rakening("rekening_nummer", "tegenrekening_nummer", chaining=True)
+#             # .fake_categories("rekening_naam", "tegenrekening_naam", chaining=True)
+#             # .fake_categy("omschrijving", chaining=True)
+#             # .show_data_frame()
+#         )
+#         print("saving")
+#         # fake_df.save(f"ano_{one_filename}")
         
-        fake_df.save(self.output_file)
+#         fake_df.save(self.output_file)
         
-if __name__ == "__main__":
-    input_file = "data1.csv"
-    output_file = "fake_customers5.csv"
+# if __name__ == "__main__":
+#     input_file = "data1.csv"
+#     output_file = "fake_customers5.csv"
     
-    anonymizer = Anonymizer(input_file, output_file)
-    anonymizer.anonymize_data()
+#     anonymizer = Anonymizer(input_file, output_file)
+#     anonymizer.anonymize_data()
+
+import glob
+
+# ... (import other necessary modules)
+
+seprator = ";"
+
+class DataLoader:
+    def __init__(self, filename):
+        self.filename = filename
+        self._load_data()
+        self.anonymizer = anonymize(self.data_df)  # Create an instance of anonymize
+    def _load_data(self):
+        self.data_df = pd.read_csv(self.filename, delimiter=seprator)
+
+    def process_data(self, anonymizer, file_name):
+        print(f'loading {self.filename}')
+        try:
+            if "omschrijving" in self.data_df.columns:  # Check if "omschrijving" column exists
+                self._process_with_omschrijving(anonymizer,file_name)
+            else:
+                self._process_without_column(anonymizer,file_name)
+            if "rekening_nummer" in self.data_df.columns:
+                self._process_with_rekening(anonymizer,file_name)
+            else:
+                self._process_without_column(anonymizer,file_name)
+        except Exception as e:
+            print("Exception occurred:", str(e))
+        
+        
+
+    def _process_with_omschrijving(self, anonymizer,file_name):
+        # an = anonymizer(self.data_df)
+        fake_df = (
+            self.anonymizer
+            .fake_categy("omschrijving", chaining=True)
+        )
+        fake_df.save(f"ano_{self.filename}")
+        
+    def _process_without_column(self, anonymizer,file_name):
+        print("Column 'omschrijving' not found in the DataFrame. Skipping 'omschrijving' column...")
+        self.anonymizer
+        fake_df = (
+            self.anonymizer
+        )
+        fake_df.save(f"ano_{self.filename}")
+
+
+    def _process_with_rekening(self, anonymizer,file_name):
+        self.anonymizer
+        fake_df = (
+            self.anonymizer
+            .fake_rakening("rekening_naam","tegenrekening_naam", chaining=True)
+        )
+        fake_df.save(f"ano_{self.filename}")
+        
+
+
+
+# Process data for each CSV file
+if __name__ == "__main__":
+    for one_filename in glob.glob(r'*.csv'):
+        loader = DataLoader(one_filename)
+        anonymizer = anonymize  # Provide the Anonymizer class here
+        loader.process_data(anonymizer, one_filename)
+      
