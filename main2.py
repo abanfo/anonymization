46e5823c3c7a6d4d0d8c6daac61d3f7d60d7c139
Abraham <abanfo81@gmail.com>
2023-08-25 14:08:32 +0200

    fixed code

--- main2.py
+++ main2.py
@@ -16,6 +16,7 @@ class DataLoader:
         self.filename = filename
         self._load_data()
         self.anonymizer = anonymize(self.data_df)  # Create an instance of Anonymizer
+
     def _load_data(self):
         self.data_df = pd.read_csv(self.filename, delimiter=seprator)
 
@@ -40,14 +41,13 @@ class DataLoader:
             # else:
             #     self._process_without_column(file_name)
 
-            # if "rekening_nummer" in self.data_df.columns and "tegenrekening_nummer" in self.data_df.columns and "rekening" in self.data_df.columns:
-            #     self._process_with_rekening(file_name, ["rekening_nummer","tegenrekening_nummer", "rekening"])
-            # elif "rekening_nummer" in self.data_df.columns and "tegenrekening_nummer" in self.data_df.columns:
-            #     self._process_with_rekening(file_name, ["rekening_nummer","tegenrekening_nummer"])
+
+            if "rekening_nummer" in self.data_df.columns or "tegenrekening_nummer" in self.data_df.columns or "rekening_nummer1" in self.data_df.columns or "tegenrekening_nummer1" in self.data_df.columns :
+                self._process_with_rekening(file_name, ["rekening_nummer","tegenrekening_nummer"])
             # elif self._process_with_rekening(file_name, ["rekening"]):
-            #     self._process_with_rekening(file_name, ["rekening"])
-            # else:
-            #     self._process_without_column(file_name)
+            #     self._process_with_rekening(file_name)
+            else:
+                self._process_without_column(file_name)
             # if "naam" in self.data_df.columns:
             #     self._process_with_name(file_name, ["naam"])
             # else:
@@ -60,17 +60,17 @@ class DataLoader:
             #     self._process_with_postcode(file_name,["postcode"])
             # else:
             #     self._process_without_column(file_name)
-            if "omschrijving" in self.data_df.columns:
-                self._process_with_omschrijven(file_name,["omschrijving"])
-            else:
-                self._process_without_column(file_name)      
+            # if "omschrijving" in self.data_df.columns:
+            #     self._process_with_omschrijven(file_name,["omschrijving"])
+            # else:
+            #     self._process_without_column(file_name)      
 
         except Exception as e:
             print("Exception occurred:", str(e))
         
 
     def _process_with_omschrijven(self,file_name,col_name):
-        fake_df = self.anonymizer.fake_omschrijven(*col_name, chaining=True)
+        fake_df = self.anonymizer.fake_omschrijven_text(*col_name, chaining=True)
         self._save_result(fake_df, file_name)    
     def _process_with_name(self,file_name,col_name):
         fake_df = self.anonymizer.fake_names(*col_name, chaining=True)
@@ -101,12 +101,100 @@ class DataLoader:
         fake_df.save(f"ano_{file_name}")
         
 
-
-
-# Process data for each CSV file
 if __name__ == "__main__":
-    for one_filename in glob.glob(r'data3.csv'):
+    all_data = []
+    anonymizer = anonymize  # Create a single instance of Anonymizer
+
+    # Load all data into a list
+    for one_filename in glob.glob(r'*.csv'):
         loader = DataLoader(one_filename)
-        anonymizer = anonymize  # Provide the Anonymizer class here
-        loader.process_data(anonymizer, one_filename)
+        all_data.append(loader.data_df)
+
+    # Combine all data into a single DataFrame
+    combined_data = pd.concat(all_data, ignore_index=True)
+
+    # Process the combined DataFrame using the same anonymizer instance
+    anonymizer_instance = anonymizer(combined_data)
+    anonymizer_instance.fake_rekening("rekening_nummer", "tegenrekening_nummer", "rekening")
+
+    # Process each individual file using the processed data
+    for filename in glob.glob(r'*.csv'):
+        loader = DataLoader(filename)
+        processed_df = loader.process_data(anonymizer_instance,filename)
+        
+        # Save the processed data
+        if processed_df is not None:
+            processed_filename = f"processed_{filename}"
+            processed_df.to_csv(processed_filename, index=False)
+            print(f"Processed data saved to: {processed_filename}")
+        else:
+            print(f"Processing failed for: {filename}")
+
+# if __name__ == "__main__":
+#     all_data = []
+#     anonymizer = anonymize  # Create a single instance of Anonymizer
+
+#     # Load all data into a list
+#     for one_filename in glob.glob(r'*.csv'):
+#         loader = DataLoader(one_filename)
+#         all_data.append(loader.data_df)
+
+#     # Combine all data into a single DataFrame
+#     combined_data = pd.concat(all_data, ignore_index=True)
+
+#     # Process the combined DataFrame using the same anonymizer instance
+#     anonymizer_instance = anonymizer(combined_data)
+#     anonymizer_instance.fake_rekening("rekening_nummer", "tegenrekening_nummer", "rekening")
+
+#     for filename in glob.glob(r'*.csv'):
+#         loader = DataLoader(filename)
+#         anonymizer = anonymize
+#         loader.process_data(anonymizer, filename)
+    # Save the processed data
+    # anonymizer_instance.save("ano_combined_data.csv")
+# Process data for each CSV file
+# if __name__ == "__main__":
+#     anonymizer = anonymize  # Provide the Anonymizer class here
+#     for one_filename in glob.glob(r'*.csv'):
+#         loader = DataLoader(one_filename)
+#         # anonymizer = anonymize  # Provide the Anonymizer class here
+#         loader.process_data(anonymizer, one_filename)
       
+# if __name__ == "__main__":
+#     all_data = []
+#     anonymizer = anonymize
+#     # Load all data into a list
+#     for one_filename in glob.glob(r'*.csv'):
+#         loader = DataLoader(one_filename)
+#         all_data.append(loader.data_df)
+       
+        
+#     # # Create a single instance of Anonymizer
+
+#     df = pd.concat(all_data)
+#     print(len(df))
+#     # anonymizer = anonymize(df)
+#     loader.process_data(anonymizer, df)
+    # anonymizer = anonymize(df)
+    # loader.process_data(anonymizer, all_data)
+    # Process data for each loaded DataFrame
+    # for one_filename in glob.glob(r'*.csv'):
+    #     loader = DataLoader(one_filename)
+    #     loader.process_data(anonymizer, one_filename)
+# if __name__ == "__main__":
+#     all_data = []
+#     anonymizer = anonymize  # Provide the Anonymizer class here
+
+#     # Load all data into a list
+#     for one_filename in glob.glob(r'bank.csv'):
+#         loader = DataLoader(one_filename)
+#         all_data.append(loader.data_df)
+
+#     # Concatenate all data frames into a single DataFrame
+#     combined_data = pd.concat(all_data, ignore_index=True)
+
+#     # Create a single instance of Anonymizer
+#     anonymizer_instance = anonymizer(combined_data)
+
+#     # Process the combined data using the anonymizer
+#     loader.process_data(anonymizer_instance, "combined_data.csv")
